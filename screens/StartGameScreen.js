fcd9427436a5adb25b276dd8bd30ec925fbfa3ca
amr4356 <amrooo73321@gmail.com>
2024-10-04 20:15:08 +0300

    52. Configuring the TextInput Field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput}  maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,17 +32,16 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
-    fontSize:32,
-    borderBottomColor: '#ddb52f',
-    borderBottomWidth:2,
-    color: '#ddb52f',
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
     marginVertical: 8,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     width: 50,
-    textAlign: 'center',
+    textAlign: "center",
     
-
-  }
+  },
 });
 
 export default StartGameScreen;
