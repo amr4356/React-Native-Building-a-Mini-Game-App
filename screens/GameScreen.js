ee8288c7f00439cf158d62b2e4bb5c6c33456505
amr4356 <amrooo73321@gmail.com>
2024-10-05 11:35:04 +0300

    61. Starting Work on the Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,23 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-function GameScreen(){
-    return <Text>
-        GameScreen
-    </Text>
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Highter or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>Log rounds</View> */}
+    </View>
+  );
 }
 
-
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
