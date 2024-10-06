e4365455052969eacd5a8f1d2636d7c8cb034082
amr4356 <amrooo73321@gmail.com>
2024-10-06 22:42:15 +0300

    82. Introducing the Dimensions API

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 function NumberContainer({ children }) {
@@ -10,20 +10,21 @@ function NumberContainer({ children }) {
 }
 
 export default NumberContainer;
+const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   constainer: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 300 ? 12 : 24,
+    margin: deviceWidth < 300 ? 12 : 24,
     borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
-    fontFamily: 'open-sans-bold'
+    fontSize: deviceWidth < 300 ? 28 : 36,
+    fontFamily: "open-sans-bold",
   },
 });
