ffb19c20e3fbae09d40b2b58797d63937bfd8f8e
amr4356 <amrooo73321@gmail.com>
2024-10-06 09:50:13 +0300

    73. Using & Styling Nested Text

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen() {
   return (
@@ -12,7 +13,11 @@ function GameOverScreen() {
           src={require("../assets/images/success.png")}
         />
       </View>
-      <Text>Your phone needed X round to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> round to guess
+        the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -22,8 +27,8 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     padding: 24,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 300,
@@ -37,5 +42,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
