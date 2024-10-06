3bc79192a35b86abf024e5717ddbdca306929fa5
amr4356 <amrooo73321@gmail.com>
2024-10-06 21:58:32 +0300

    81. Setting Dynamic Widths

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -7,12 +7,14 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: 24,
-    color: 'white',
+    color: "white",
     textAlign: "center",
     borderWidth: 2,
-    borderColor: 'white',
+    borderColor: "white",
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
